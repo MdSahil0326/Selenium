7ab4fcdfb9ed13be17ab84e97e789233b006084f
MdSahil0326 <mohammedsahil0026@gmail.com>
2026-08-29 13:10:09 +0530

    update

--- src/test/java/WebDriver/NavigatetoUrlMethod.java
+++ src/test/java/WebDriver/NavigatetoUrlMethod.java
@@ -1,12 +1,12 @@
 package WebDriver;
-import java.net.URL;
+//import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NavigatetoUrlMethod {
 	public static void main(String[] args)throws Exception {
 		WebDriver driver= new ChromeDriver();
-		driver.navigate().to(new URL("https://www.flipkart.com/"));
+	//	driver.navigate().to(new URL("https://www.flipkart.com/"));
 		driver.navigate().back();
 		Thread.sleep(2000);
 		driver.navigate().forward();
